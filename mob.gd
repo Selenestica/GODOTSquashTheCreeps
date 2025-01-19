adb3e5e7859e3521f2f36ea5543bb4bfcbb3b3e0
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2025-01-19 16:52:46 -0500

    added spawn location and cylinders

--- mob.gd
+++ mob.gd
@@ -14,3 +14,15 @@ func initialize(start_position, player_position):
 	# Rotate this mob randomly within range of -45 and +45 degrees,
 	# so that it doesn't move directly towards the player.
 	rotate_y(randf_range(-PI / 4, PI / 4))
+	
+	# We calculate a random speed (integer)
+	var random_speed = randi_range(min_speed, max_speed)
+	# We calculate a forward velocity that represents the speed.
+	velocity = Vector3.FORWARD * random_speed
+	# We then rotate the velocity vector based on the mob's Y rotation
+	# in order to move in the direction the mob is looking.
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
+
+
+func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
+	queue_free()
